a71d8009e2cef9c3d47f1396a0adba57341df560
Araceli Soffulto <34224902+chelyx@users.noreply.github.com>
2021-02-16 23:35:32 -0300

    Create semantic.h

---
+++ TP5/semantic.h
@@ -0,0 +1,10 @@
+void iniciar(void);
+void terminar(void);
+int declarar(char* id);
+void asignar(char* id);
+void leer(char* id);
+void escribir(char* id);
+int existe_identificador(char* id);
+void generar_infijo(int op, char* id1, char* id2);
+void generar_unario(char* id);
+bool contiene_nodo(char* id);
